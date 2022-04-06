7d8b730c6ffe1773c39b036e5f165ec5c4a11bcd
Draic_Kin <forinstance7@gmail.com>
2022-04-06 21:45:12 +0300

    Start Game and Win Conditions

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -4,6 +4,7 @@
 #include "ToonTanksGameMode.h"
 #include "Tank.h"
 #include "Tower.h"
+#include "ToonTanksPlayerController.h"
 #include "Kismet/GameplayStatics.h"
 
 void AToonTanksGameMode::ActorsDeath(AActor* DeadActor)
@@ -11,20 +12,49 @@ void AToonTanksGameMode::ActorsDeath(AActor* DeadActor)
     if (DeadActor == Tank)
     {
         Tank -> HandlePawnDeath();
-        if(Tank -> GetPlayerControllerRef())
+        if(ToonTanksPlayerController)
         {
-            Tank -> DisableInput(Tank -> GetPlayerControllerRef());
-            Tank -> GetPlayerControllerRef() -> bShowMouseCursor = false;
-        }     
+            ToonTanksPlayerController -> SetPlayerEnabledState(false);
+        }
+	    GameOver(false);
     }
     else if (ATower* DeadTower = Cast<ATower>(DeadActor))
     {
         DeadTower -> HandlePawnDeath();
+        --TowerCount;
+        if (TowerCount == 0) GameOver(true);
     }
 }
 
 void AToonTanksGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+    HandleGameStart();
+}
+
+void AToonTanksGameMode::HandleGameStart()
+{
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+    ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));   
+
+    TowerCount = GetTowerCount();
+    StartGame();
+    if (ToonTanksPlayerController)
+    {
+        ToonTanksPlayerController->SetPlayerEnabledState(false);
+    
+        FTimerDelegate TimerDelegateStart = FTimerDelegate::CreateUObject
+                                            (ToonTanksPlayerController, 
+                                            &AToonTanksPlayerController::SetPlayerEnabledState, 
+                                            true);
+        FTimerHandle StartHandle;
+        GetWorldTimerManager().SetTimer(StartHandle, TimerDelegateStart, StartDelay, false);
+    }
+}
+
+int32 AToonTanksGameMode::GetTowerCount() const
+{
+    TArray <AActor*> Towers;
+    UGameplayStatics::GetAllActorsOfClass(this, ATower::StaticClass(), Towers);
+    return Towers.Num();
 }
--- Source/ToonTanks/ToonTanksGameMode.h
+++ Source/ToonTanks/ToonTanksGameMode.h
@@ -20,8 +20,22 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartGame();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void GameOver(bool bWonGame);
 
 private:
 
 	class ATank* Tank;
+	class AToonTanksPlayerController* ToonTanksPlayerController;
+
+	void HandleGameStart();
+
+	UPROPERTY(EditAnywhere)
+	float StartDelay = 3.f;
+
+	int32 TowerCount = 0;
+	int32 GetTowerCount() const;
 };
